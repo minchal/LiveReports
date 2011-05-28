c45ed1b9ffcb05277f89ee19fee857dc9d6ba893
Michal Oles <michal.oles@gmail.com>
2011-05-29 00:49:47 +0200

    Dodano logowanie

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+﻿class SessionsController < ApplicationController
+
+def new
+end
+
+def create
+  user = User.authenticate(params[:email], params[:password])
+  if user
+    session[:user_id] = user.id
+    redirect_to root_url, :notice => "Zalogowano!"
+  else
+    flash.now.alert = "Błędny email lub hasło"
+    render "new"
+  end
+end
+
+def destroy
+  session[:user_id] = nil
+  redirect_to root_url, :notice => "Poprawnie wylogowano!"
+end
+
+end
---
+++ db/migrate/20110528164420_dodaniesoli.rb
@@ -0,0 +1,9 @@
+class Dodaniesoli < ActiveRecord::Migration
+  def self.up
+    add_column :users, :password_salt, :string
+  end
+ 
+  def self.down
+    remove_column :users, :password_salt
+  end
+end
